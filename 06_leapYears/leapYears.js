98c1459fcc4688567fe67b12def1053610ec2183
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-04 21:56:41 +0100

    Solve 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((year % 100) === 0) {
+        if ((year % 400) === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    return (year % 4) === 0;
 };
 
 // Do not edit below this line
