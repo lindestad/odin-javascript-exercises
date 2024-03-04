9f17878b1dd239a590667143769c0279f3bf19f3
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-04 20:59:55 +0100

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else if (num === 0) {
+        return '';
+        
+    }
+    let outStr = ''
+    for (let i = 0; i<num; i++) {
+        outStr = outStr.concat(string);
+    }
+    return outStr;
 };
 
 // Do not edit below this line
