5c820f879391617c2909597c82ef9e6e165d7d60
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-04 22:23:26 +0100

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function roundToOneDigit(n) {
+  return Math.round(n * 10) / 10;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * (5/9);
+  return roundToOneDigit(celcius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (celcius * (9/5)) + 32;
+  return roundToOneDigit(fahrenheit);
 };
 
 // Do not edit below this line
