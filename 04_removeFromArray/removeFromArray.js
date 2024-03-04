5e12559c16d0cb636f004f909afdb8db1929fb8c
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-04 21:39:24 +0100

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    for (let element of remove) {
+        for (let index = arr.indexOf(element); index != -1; index = arr.indexOf(element)) { // handle duplicate elements in arr
+            if (index != -1) {
+                arr.splice(index, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
