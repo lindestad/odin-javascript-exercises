3658426ed6b119d83a12e8c2adb5fbeb5e2115d4
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-04 21:50:37 +0100

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((typeof num1 != 'number') || (typeof num2 != 'number')) { 
+        // input is not a number
+        return 'ERROR';
+    }
+    if ((num1 < 0) || (num2 < 0)) {
+        // input less than 0
+        return 'ERROR';
+    }
+    let sum = 0;
+    for (let x = Math.min(num1, num2); x <= Math.max(num1, num2); x++) {
+        sum += x;
+    }
+    return sum;
 };
 
 // Do not edit below this line
