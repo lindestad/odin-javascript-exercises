daf49b18907f6d8edb722e95603bfd3cae1e9b6f
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-07 13:46:37 +0100

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    console.log(`String: ${str}`);
+    str = str.replaceAll(/ |!|\.|&|'|,/g, '');
+    console.log(`String after regex: ${str}`);
+    str = str.toLowerCase();
+    console.log(`String after lowercase: ${str}`);
+    let reverse = str.split('').reverse('').join('');
+    return str === reverse;
 };
 
 // Do not edit below this line
