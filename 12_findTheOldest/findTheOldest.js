f47ff760aad3c8086579783da422507141e3c18c
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-07 15:23:01 +0100

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // add current year as date of death if no date of death to simplify,
+  // tests do not care for this, but would be erronous in real world data
+  // as entire object is returned
+  people = people.map((person) => {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    return person;
+  });
 
+  people = people.sort((a, b) => {
+    const lastAge = a.yearOfDeath - a.yearOfBirth;
+    const nextAge = b.yearOfDeath - b.yearOfBirth;
+    return lastAge > nextAge ? -1 : 1;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
