1fa7b6dcaf4f8427008dce44ab7b131535bcea76
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-07 14:24:57 +0100

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(target) {
+    const max_iterations = 1000;
+    if (target>= max_iterations) {
+        throw Error("Exceeded loop iteration limit.")
+    }
+    target = Number(target);
+    if (target < 0) {
+        return "OOPS";
+    }
+    if (target === 0) {
+        return 0;
+    }
+    let next = 0;
+    let current = 1;
+    let previous = 1;
+    for (let i=2; i<target; i++) {
+        next = current + previous;
+        previous = current;
+        current = next;
+    }
+    return current;
 };
 
 // Do not edit below this line
