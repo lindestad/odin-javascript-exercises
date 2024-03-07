9647dbb4f87ae134c7421d3a951cd1f2fbe3be01
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-07 13:12:09 +0100

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce( (a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(i) {
+  if (i === 0) {
+    return 1;
+  }
+  let ans = 1;
+  for (let x = i; x>0; --x) {
+    console.log(x);
+    ans = ans * (x);
+  }
+  return ans;
 };
 
 // Do not edit below this line
